0be91f4a06fb6c982f9491b8f132d96f64964cf8
Aries Lam <tranglam@ariess-macbook-pro.local>
2019-03-26 13:36:26 -0700

    Initial Commit

---
+++ My_TodoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My_TodoList
+//
+//  Created by Aries Lam on 3/26/19.
+//  Copyright © 2019 Cuong Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
